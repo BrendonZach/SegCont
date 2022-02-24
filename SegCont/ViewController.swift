3c21dc50cea66f297d6a9920c88c1ca74be29a68
Brendon Zach <brendon_zach@outlook.com>
2022-02-24 13:02:57 -0700

    Initial Commit

---
+++ SegCont/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegCont
+//
+//  Created by Brendon Zach on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
